996d623f5d53bd01b5edc0dc0eb46e486143ad7e
CbdFocus <baodong.cao2@wework.com>
2019-10-30 11:17:42 +0800

    FEATURE  - Add seeds

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+
+  gem 'ffaker', '~> 2.13'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'ffaker'
+
+5.times do
+  user = User.create(email: FFaker::Internet.email)
+  (rand(3)).times { user.pets.create(name: FFaker::Name.first_name) }
+end
